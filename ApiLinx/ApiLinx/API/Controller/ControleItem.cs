7b6c2f10b9c6a506986b10512b1943e1c475a162
PedroReis99 <pmreis6@gmail.com>
2020-03-07 13:05:11 -0300

    API desafio VS2019

---
+++ ApiLinx/ApiLinx/API/Controller/ControleItem.cs
@@ -0,0 +1,42 @@
+﻿using ApiLinx.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiLinx.API.Data;
+
+namespace ApiLinx.API.Controller
+{
+    public class ControleItem :ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<ItemImportado>>> Get([FromServices] Data.DataContext context)
+        {
+            var produto = await context.Produtos.ToListAsync();
+            return produto;
+        }
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<ItemImportado>> Post(
+            [FromServices] DataContext context,
+            [FromBody] ItemImportado model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Produtos.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
